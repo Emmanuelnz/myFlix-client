cff6a929ddf51e1d57abe0eee71f8b6ede82e8cb
Emmanuelnz <emmanuelnun75@gmail.com>
2022-08-16 03:11:44 -0400

    used react bootstrap to remake registration form

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { Form, Button, Card, CardGroup, Container, Col, Row, } from "react-bootstrap";
+
 
 import './registration-view.scss';
+import CardHeader from 'react-bootstrap/esm/CardHeader';
 
 export function RegistrationView(props) {
   const [ username, setUsername ] = useState('');
@@ -16,21 +19,42 @@ export function RegistrationView(props) {
   };
 
   return (
-    <form>
-      <label> Username:
-        <input type='text' value={username} onChange={e => setUsername(e.target.value)} /> 
-      </label>
-      <label> Password: 
-        <input type='password' value={password} onChange={e => setPassword(e.target.value)} /> 
-      </label>
-      <label> Email: 
-        <input type='email' value={email} onChange={e => setEmail(e.target.value)} />
-      </label>
-      <label> Birthday: 
-        <input type='date' value={birthDay} onChange={e => setBirthday(e.target.value)} /> 
-      </label>
-      <button className='registerbtn' type='submit' onClick={handleSubmit}>Register</button>
-    </form>
+    <Container>
+      <Row>
+        <Col>
+          <CardGroup>
+            <Card className='registerbdy'>
+              <Card.Body>
+                <Card.Title className='register-title'>New myFlix User</Card.Title>
+                <Form>
+                  <Form.Group controlId='formUsername'>
+                    <Form.Label>Username: </Form.Label>
+                    <Form.Control type='text' onChange={e => setUsername(e.target.value)} Placeholder="Enter username (username must be at least 5 characters) " />
+                  </Form.Group>
+
+                  <Form.Group controlId='formPassword'>
+                    <Form.Label>Password: </Form.Label>
+                    <Form.Control type='password' onChange={e => setPassword(e.target.value)} Placeholder='Enter password' />
+                  </Form.Group>
+
+                  <Form.Group controlId='formEmail'>
+                    <Form.Label>Email: </Form.Label>
+                    <Form.Control type='email' onChange={e => setPassword(e.target.value)} Placeholder='Enter Email address'/>
+                  </Form.Group>
+
+                  <Form.Group controlId='formBirthday'>
+                    <Form.Label>Birthday: </Form.Label>
+                    <Form.Control type='date' onChange={e => setPassword(e.target.value)} />
+                  </Form.Group>
+
+                  <Button variant='primary' type='submit' onClick={handleSubmit}>Register</Button>
+                </Form>
+              </Card.Body>
+            </Card>
+          </CardGroup>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 

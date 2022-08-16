9e7a06a99f3fa798db5b1de3f3f8e3bc725902b9
Emmanuelnz <emmanuelnun75@gmail.com>
2022-08-16 03:12:32 -0400

    used react bootstrap to remake movie view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,41 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Card, Container, Col, Row, Button } from 'react-bootstrap';
 
 
 export class MovieView extends React.Component {
 
    render() {
     const { movie, onBackClick } = this.props;
-
+  
     return (
-      <div className='movie-view'>
-        <div className='movie-poster'>
-          <img crossOrigin='anonymous' src={movie.ImagePath} />
-        </div>
-        <div className='movie-title'>
-          <span className='label'>Title: </span>
-          <span className='value'> { movie.Title } </span>
-        </div>
-        <div className='movie-description'>
-          <span className='label'>Description: </span>
-          <span className='value'> { movie.Description } </span>
-        </div>
-        <div className='movie-directors'>
-          <span className='label'>Directors: </span>
-          <span className='value'>{ movie.Directors.Name } </span>
-        </div>
-        <div className='movie-genre'>
-          <span className='label'>Genre: </span>
-          <span className='value'> { movie.Genre.Name } </span>
-        </div>
-        <button onClick={() => { onBackClick(null); }}>Back</button>
-      </div>
-    );
+      <Container fluid>
+        <Row>
+          <Col>
+            <Card bg='dark' text='light'>
+              <Card.Img variant='top' crossOrigin='anonymous' src={movie.ImagePath} />
+              <Card.Body>
+                <Card.Title className='mview-title'> { movie.Title } </Card.Title>
+                <Card.Text>
+                  <span className='mview-text'>Description: </span> { movie.Description } </Card.Text>
+                <Card.Text><span className='mview-text'>Directors: </span>{ movie.Directors.Name } </Card.Text>
+                <Card.Text><span className='mview-text'>Genre: </span>{ movie.Genre.Name } </Card.Text>
+                <Button variant='info' onClick={ () => {onBackClick(null); }}>Back</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    )
+
    }
 }
 
 MovieView.propTypes = {
   movie: PropTypes.shape({
+    ImagePath: PropTypes.string.isRequired,
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
 
@@ -49,7 +47,6 @@ MovieView.propTypes = {
       Name: PropTypes.string.isRequired,
       Description: PropTypes.string.isRequired
     }),
-    ImagePath: PropTypes.string.isRequired
   }).isRequired,
   onBackClick: PropTypes.func.isRequired
 };

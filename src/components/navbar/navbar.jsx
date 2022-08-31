e5befd175b420b01c90ba1e6d211ff0a6663e2ed
Emmanuelnz <emmanuelnun75@gmail.com>
2022-08-31 09:01:48 -0400

    navbar styling and color changes

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -26,18 +26,33 @@ export function NavBar({ user }) {
   }
 
   return (
-    <Navbar className='main-nav' sticky='top' bg='dark' expand='xxl' variant='dark'>
+    <Navbar 
+      className='main-nav' 
+      sticky='top' 
+      bg='dark' 
+      expand='xxl' 
+      variant='dark'>
       <Container fluid>
         <Button variant='info' href='/' >myFlix</Button>
         <Navbar.Toggle aria-controls='offcanvasNavbar-expand' />
-          <Navbar.Offcanvas>
+          <Navbar.Offcanvas className='sidenav'>
             <Offcanvas.Header closeButton>
-              <Offcanvas.Title>myFlix</Offcanvas.Title>
+              <Offcanvas.Title>
+              <Button 
+                variant='dark' 
+                href='/' 
+                >myFlix
+              </Button>
+              </Offcanvas.Title>
             </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className='ms-auto'>
                   {isAuth() && (
-                    <Nav.Link href={`/users/${user}`}>{user}</Nav.Link>
+                    <Button 
+                    variant='outline-dark'
+                    href={`/users/${user}`}>
+                      {user}
+                    </Button>
                   )}
 
                   {!isAuth() && (
@@ -49,9 +64,12 @@ export function NavBar({ user }) {
                   )}
                   
                   {isAuth() && (
-                    <Button variant='info' onClick={() => {
-                      this.onLogOut()
-                    }}>Log out</Button>
+                    <Button
+                      className='mt-3'
+                      variant='dark' 
+                      onClick={() => {this.onLogOut()}}
+                      >Log out
+                    </Button>
                   )}
                 </Nav>
               </Offcanvas.Body>
